5d1f741f21f1310a3387e73e31f828887289d9f4
Andrea Lai <laidevpub@gmail.com>
2017-08-25 13:21:09 +0200

    Json tree viewer -experiment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import JSONViewer from 'react-json-viewer';
+import JSONTree from 'react-json-tree'
+import livDue from './liv_due.json';
+
+
+let data = [
+  {liv2: "A1.1", desc:"_a1.1", keyIs: "liv2", in: "A1"},
+  {liv2: "A1.2", desc:"_a1.2", keyIs: "liv2", in: "A1"},
+  {liv1: "A1", desc:"_a1", keyIs: "liv1", in: "A"},
+  {liv1: "B1", desc:"_b1", keyIs: "liv1", in: "B"},
+  {liv0: "A", desc:"_a1", keyIs: "liv0", in: ""},
+  {liv0: "B", desc:"_b1", keyIs: "liv0", in: ""},
+];
+
+
+const take = (array, n) => {
+  var newArray = [];
+  for (var index = 0; index < n ; index++) {
+    var element = array[index];
+    newArray.push(element);
+  }
+  return newArray;
+}
+
+const trasforma = (arr, sKey) => {
+  let obj = {};
+  arr.forEach(element => {
+    let nameOfKey = element[sKey];
+    let keyObj = element[nameOfKey];
+    obj[keyObj] = element;
+  });
+  return obj;
+}
+
+data = trasforma(data, "keyIs");
+
+
+console.log(data);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <JSONTree data={data}/>
+       <JSONViewer json={data}/>
       </div>
     );
   }
