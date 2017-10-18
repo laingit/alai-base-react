520e02a9bd8f3d38773f8851fafdc32c27d885ff
Andrea Lai <laidevpub@gmail.com>
2017-10-18 12:13:02 +0200

    TEST IDROMTRI

--- src/App.js
+++ src/App.js
@@ -25,8 +25,61 @@ const store = createStore(combineReducers({
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
    applyMiddleware(thunkMiddleware));
 
+// var vectorLayer =  new ol.layer.Image({
+// source: new ol.source.ImageVector({
+//     source: new ol.source.Vector({
+//     url: 'http://localhost:8080/geoserver/Arpas/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Arpas:idropost&maxFeatures=200&outputFormat=application%2Fjson&srsName=EPSG:3857',
+//     format: new ol.format.GeoJSON(),
+//     }),
+//     style: new ol.style.Style({
+//       image: new ol.style.Circle({
+//         fill: new ol.style.Fill({
+//           color: 'rgba(255,0,0,0.8)'
+//         }),
+//         radius: 3,
+//         stroke: new ol.style.Stroke({
+//           color: '#000',
+//           width: 1
+//         })
+//       })
+//     })
+// }),
+// });
 
-store.dispatch(mapActions.setView([-7, 40], 4));
+const addDataFromGeoJSON = (url, sourceName) => {
+  // Fetch URL
+  return fetch(url)
+    .then(
+      response => response.json(),
+      error => console.error('An error occured.', error),
+    )
+    // addFeatures with the features, source name
+    // .then(json => store.dispatch(mapActions.addFeatures(sourceName, json)));
+    .then(json => {
+      store.dispatch(mapActions.addSource(sourceName, {
+        type: 'geojson',
+        data: json
+      }));
+      store.dispatch(mapActions.addLayer({
+        id: sourceName,
+        source: sourceName,
+        paint: {
+          'circle-radius': 5,
+          'circle-color': '#f46b42',
+          'circle-stroke-color': '#3a160b',
+        }
+      }));
+    });
+};
+
+const addIdrometri = () => {
+  let url = 'http://localhost:8080/geoserver/Arpas/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Arpas:idropost&maxFeatures=200&outputFormat=application%2Fjson&srsName=EPSG:3857';
+  let sourceName = 'idrometri'
+  addDataFromGeoJSON(url, sourceName);
+}
+
+
+store.dispatch(mapActions.setView([9, 40], 8));
 
 // add the OSM source
 store.dispatch(mapActions.addSource('osm', {
@@ -57,10 +110,10 @@ store.dispatch(mapActions.addLayer({
 // retrieve GetCapabilities and give user ability to add a layer.
 const addWMS = () => {
   // this requires CORS headers on the geoserver instance.
-  const urlBound = 'https://demo.boundlessgeo.com/geoserver/wms?service=WMS&request=GetCapabilities';
-  const urlLocal = 'http://192.168.18.51:8090/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities';
-  const urlRegione = 'http://webgis.regione.sardegna.it/geoserver/ows?service=WMS&request=GetCapabilities';
-  fetch(urlBound)
+  //const urlBound = 'https://demo.boundlessgeo.com/geoserver/wms?service=WMS&request=GetCapabilities';
+  const urlLocal = 'http://192.168.18.51:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities';
+  //const urlRegione = 'http://webgis.regione.sardegna.it/geoserver/ows?service=WMS&request=GetCapabilities';
+  fetch(urlLocal)
   .then(
     response => response.text(),
     error => console.error('An error occured.', error),
@@ -71,13 +124,13 @@ const addWMS = () => {
     ReactDOM.render(<AddWMSLayer
       onAddLayer={(layer) => {
         // add a new source and layer
-        const getMapUrlBound = `https://demo.boundlessgeo.com/geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
-        const getMapUrlLocal = `http://192.168.18.51:8090/geoserver/ows?service=wms&version=1.3.0&request=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
-        const getMapUrlRegione = `http://webgis.regione.sardegna.it/geoserver/ows?service=WMS&request=GetCapabilities&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
+        //const getMapUrlBound = `https://demo.boundlessgeo.com/geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
+        const getMapUrlLocal = `http://192.168.18.51:8080/geoserver/ows?service=wms&version=1.3.0&request=GetMap&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
+        //const getMapUrlRegione = `http://webgis.regione.sardegna.it/geoserver/ows?service=WMS&request=GetCapabilities&FORMAT=image/png&TRANSPARENT=TRUE&SRS=EPSG:900913&LAYERS=${layer.Name}&STYLES=&WIDTH=256&HEIGHT=256&BBOX={bbox-epsg-3857}`;
         store.dispatch(mapActions.addSource(layer.Name, {
           type: 'raster',
           tileSize: 256,
-          tiles: [getMapUrlBound],
+          tiles: [getMapUrlLocal],
         }));
         store.dispatch(mapActions.addLayer({
           metadata: {
@@ -96,10 +149,7 @@ const addWMS = () => {
 
 const App = () => (
   <div>
-
-  <SdkMap  styles={{width: '800px',
-              fontSize: '16px',
-              backgroundColor: '#fff'}}
+  <SdkMap  
     includeFeaturesOnClick
     onClick={(map, xy, featuresPromise) => {
       // show a popup containing WMS GetFeatureInfo.
@@ -125,6 +175,7 @@ const App = () => (
           />);
         }
       });
+
     }}
     store={store}
   />
@@ -132,7 +183,9 @@ const App = () => (
   <div>
     <h4>Layers</h4>
     <LayerList store={store} />
+    
     <button onClick={addWMS}>Add WMS Layer</button>
+    <button onClick={addIdrometri}>Add IDROMETRI Layer</button>
   </div>
 </div>
 );
